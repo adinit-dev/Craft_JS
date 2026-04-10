9c22ada7607b6934daa429f9398916cf12435e1e
Aditya Singh <adsingh84000@gmail.com>
2026-04-10 17:33:31 +0530

    added project day 3

---
+++ playing_css_with_JS/script.js
@@ -0,0 +1,18 @@
+
+const input = document.querySelectorAll(".control input");
+
+function handleChange(){
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+input.forEach(input => {
+    input.addEventListener('change', handleChange)
+});
+input.forEach(input => {
+
+ input.addEventListener('mousemove', handleChange)
+});
+
+
+
